e24532c552034fe207a2865f9428b0f801bbea4c
fixvas <fixvas@gmail.com>
2017-06-10 16:45:49 +0300

    2nd iteration of leap.py

--- leap/leap.py
+++ leap/leap.py
@@ -2,13 +2,5 @@
 
 
 def is_leap_year(year):
-    if (year % 4) != 0:
-        return False
-    else:
-        if (year % 100) != 0:
-            return True
-        else:
-            if (year % 400) == 0:
-                return True
-            else:
-                return False
+    return (year % 400 == 0) or (year % 4 == 0) and (year % 100 != 0)
+
