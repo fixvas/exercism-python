a7da0473d5b28cc6ca6b5900216ba4bd25be88b7
fixvas <fixvas@gmail.com>
2017-06-11 14:12:50 +0300

    Exercise Isogram is done

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,1 +1,13 @@
-def is_isogram():
+import string
+
+
+def is_isogram(word):
+    word_lowercase = word.lower()
+    d = {} 
+    for letter in word_lowercase:
+        if letter in string.ascii_lowercase:
+            if letter in d:
+                return False
+            else:
+                d[letter] = 1
+    return True
