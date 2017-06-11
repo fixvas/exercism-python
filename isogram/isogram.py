76c0fd8d6dc71c6d813443e9ecaa3afde620a342
fixvas <fixvas@gmail.com>
2017-06-11 14:58:51 +0300

    Minor refactoring isogram.py

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,11 +1,8 @@
-import string
-
-
 def is_isogram(word):
     word_lowercase = word.lower()
     d = {} 
     for letter in word_lowercase:
-        if letter in string.ascii_lowercase:
+        if letter.isalpha():
             if letter in d:
                 return False
             else:
