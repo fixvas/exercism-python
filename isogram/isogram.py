53d7b62c0a557c467aa7631af54871b408810804
fixvas <fixvas@gmail.com>
2017-06-11 17:24:29 +0300

    2nd iteration of Isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,10 +1,6 @@
 def is_isogram(word):
     word_lowercase = word.lower()
-    d = {} 
     for letter in word_lowercase:
-        if letter.isalpha():
-            if letter in d:
-                return False
-            else:
-                d[letter] = 1
+        if letter.isalpha() and word_lowercase.count(letter) > 1:
+            return False
     return True
