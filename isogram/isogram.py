80f550559a4fc7e498b2fd360e2f5e35986ce1f3
fixvas <fixvas@gmail.com>
2017-06-12 13:26:33 +0300

    3rd iteration of Isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,6 +1,3 @@
 def is_isogram(word):
-    word_lowercase = word.lower()
-    for letter in word_lowercase:
-        if letter.isalpha() and word_lowercase.count(letter) > 1:
-            return False
-    return True
+    symbols = list(filter(str.isalpha, word.lower()))
+    return len(symbols) == len(set(symbols))
