feb17cbc9e98d32b384bfe2a1185e5968b4e25ba
fixvas <fixvas@gmail.com>
2017-06-12 22:20:44 +0300

    Exercise Pangram is done

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,8 @@
-def is_pangram():
-    pass
+import string
+
+
+def is_pangram(sentence):
+    for char in string.ascii_lowercase:
+        if char not in sentence.lower():
+            return False
+    return True
