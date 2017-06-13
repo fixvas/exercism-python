e102ec47dcd567bd4af7744883a1840ce80e30bb
fixvas <fixvas@gmail.com>
2017-06-13 19:02:56 +0300

    2nd iteration of Pangram

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -2,7 +2,8 @@ import string
 
 
 def is_pangram(sentence):
+    s = sentence.lower()
     for char in string.ascii_lowercase:
-        if char not in sentence.lower():
+        if char not in s:
             return False
     return True
