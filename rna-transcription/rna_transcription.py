1eaaa66e4815852770a8afd3564eaf1165d53a3f
fixvas <fixvas@gmail.com>
2017-06-14 20:57:47 +0300

    3rd iteration of rna-transcription

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,6 +1,5 @@
-def to_rna(dna):
-    errors = ['X', 'U', '']
-    for c in dna:
-        if c in errors:
+def to_rna(dna, dna_nucl='ACGT', rna_nucl='UGCA', errors='XU'):
+    for c in errors:
+        if c in dna:
             return ''
-    return dna.translate(str.maketrans('ACGT', 'UGCA'))
+    return dna.translate(str.maketrans(dna_nucl, rna_nucl))
