7c994d64c5b79a098638061d3b8aa9487c051051
fixvas <fixvas@gmail.com>
2017-06-14 17:26:02 +0300

    2nd iteration of rna-transcription

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,18 +1,6 @@
 def to_rna(dna):
     errors = ['X', 'U', '']
-    rna = ''
     for c in dna:
         if c in errors:
             return ''
-        if c == 'A':
-            rna += 'U'
-            continue
-        if c == 'C':
-            rna += 'G'
-            continue
-        if c == 'G':
-            rna += 'C'
-            continue
-        if c == 'T':
-            rna += 'A'
-    return rna
+    return dna.translate(str.maketrans('ACGT', 'UGCA'))
