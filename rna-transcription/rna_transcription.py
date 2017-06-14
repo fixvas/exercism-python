8bbb538290ccb25d49add80877988a26800b2e8a
fixvas <fixvas@gmail.com>
2017-06-14 15:50:40 +0300

    Exercise rna-transcription is done

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,18 @@
-def to_rna():
-    pass
+def to_rna(dna):
+    errors = ['X', 'U', '']
+    rna = ''
+    for c in dna:
+        if c in errors:
+            return ''
+        if c == 'A':
+            rna += 'U'
+            continue
+        if c == 'C':
+            rna += 'G'
+            continue
+        if c == 'G':
+            rna += 'C'
+            continue
+        if c == 'T':
+            rna += 'A'
+    return rna
