f0d4dfe662c44cb113e8c9a2c7ce7ead9615f818
fixvas <fixvas@gmail.com>
2017-06-15 21:36:43 +0300

    Exercise Hamming is done

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,8 @@
-def distance():
-    pass
+def distance(strand_one, strand_two):
+    distance = 0
+    if len(strand_one) != len(strand_two):
+        raise ValueError
+    for c, k in zip(strand_one, strand_two):
+        if c != k:
+            distance += 1
+    return distance
