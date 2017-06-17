ed4c2d72fe7d77ec2b8d248706f9758c5ca5ed56
fixvas <fixvas@gmail.com>
2017-06-17 18:44:13 +0300

    3rd iteration of Word-Count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,6 +1,6 @@
 def word_count(phrase):
     word_count = {}
-    words = ''.join([w if w.isalnum() else ' ' for w in phrase.lower()])
+    words = ''.join(c if c.isalnum() else ' ' for c in phrase.lower())
     words = words.split()
     for word in words:
         word_count[word] = words.count(word)
