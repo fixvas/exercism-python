cd85666e2aba545f4f0c0ec098052a10397fe2d8
fixvas <fixvas@gmail.com>
2017-06-17 13:27:10 +0300

    Exercise Word-Count is done

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,12 @@
-def word_count():
-    pass
+# from string import whitespace
+
+
+def word_count(phrase):
+    word_count = {}
+    for word in phrase.lower().replace(',', ' ').replace('_', ' ').split():
+        word = ''.join(filter(str.isalnum, word))
+        if word and word not in word_count:
+            word_count[word] = 1
+        elif word:
+            word_count[word] += 1
+    return word_count
