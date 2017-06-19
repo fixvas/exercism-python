0b5bfb08aa50bedb0abdfe293acc4784a3533b65
fixvas <fixvas@gmail.com>
2017-06-19 18:38:18 +0300

    Exercise Bob is done

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,12 @@
-def hey():
-    pass
+import re
+
+
+def hey(phrase):
+    phrase = phrase.strip()
+    if re.search(r'[A-Z]{4}', phrase) or re.search(r'[A-Z]{2}!$', phrase):
+        return "Whoa, chill out!"
+    elif re.search(r'[?]$', phrase) and not re.search(r'[A-Z]{4}', phrase):
+        return "Sure."
+    elif phrase == "":
+        return "Fine. Be that way!"
+    return "Whatever."
