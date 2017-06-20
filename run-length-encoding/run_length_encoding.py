34bcc3d991235e89ec1c507da4fd9ab3db70d4dc
fixvas <fixvas@gmail.com>
2017-06-20 19:40:49 +0300

    1st iteration of RLE

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,25 @@
-def decode():
-    pass
+def decode(line):
+    cnt = ''
+    decoded_line = ''
+    for char in line:
+        if char.isdigit():
+            cnt += char
+        else:
+            decoded_line += ((char * int(cnt)) if cnt else char)
+            cnt = ''
+    return decoded_line
 
 
-def encode():
-    pass
+def encode(line):
+    sym = []
+    encoded_line = ''
+    for char in line:
+        if not sym or char not in sym[-1]:
+            sym.append((char, 1))
+        else:
+            last = sym.pop(-1)
+            sym.append((char, last[1] + 1))
+    for char, cnt in sym:
+        code = (char if cnt == 1 else str(cnt) + char)
+        encoded_line += code
+    return encoded_line
