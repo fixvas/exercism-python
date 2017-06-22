833c748446823644eda579f0c57c03c239945452
fixvas <fixvas@gmail.com>
2017-06-22 19:48:16 +0300

    3rd iteration of Meetup

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -3,7 +3,7 @@ from datetime import date
 
 
 TEENTH_DAYS = set([13, 14, 15, 16, 17, 18, 19])
-DESCRIPTORS = {'1st': 0, '2nd': 1, '3rd': 2, '4th': 3, '5th': 4}
+DESCRIPTORS = {'1st': 0, '2nd': 1, '3rd': 2, '4th': 3, '5th': 4, 'last': -1}
 
 
 class MeetupDayException(Exception):
@@ -19,8 +19,6 @@ def meetup_day(year, month, weekday, des):
         
     if des == 'teenth':
         return date(year, month, set(days).intersection(TEENTH_DAYS).pop())
-    if des == 'last':
-        return date(year, month, days[-1])
     if des in DESCRIPTORS:
         try:
             return date(year, month, days[DESCRIPTORS[des]])
