400e633b3485a8d6f9adcff2cbd3ead71838791f
fixvas <fixvas@gmail.com>
2017-06-23 17:38:06 +0300

    1st iteration of Rotational-Cipher

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,23 @@
-def rotate():
-    pass
+from string import ascii_lowercase, ascii_uppercase
+
+
+def rotate(phrase, key):
+    coded_phrase = []
+    for char in phrase:
+        if char.islower():
+            x = ascii_lowercase.index(char)
+            y = (x + key)
+            if y > 25:
+                y -= 26
+            y = y % 26
+            coded_phrase.append(ascii_lowercase[y])
+        elif char.isupper():
+            x = ascii_uppercase.index(char)
+            y = (x + key)
+            if y > 26:
+                y -= 26
+            y = y % 26
+            coded_phrase.append(ascii_uppercase[y])
+        else:
+            coded_phrase.append(char)
+    return ''.join(coded_phrase)
