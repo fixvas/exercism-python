00c3c7c85afd7cf4abd75edf6b47778bce8670aa
fixvas <fixvas@gmail.com>
2017-06-24 17:45:45 +0300

    1st iteration of Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,8 @@
-def detect_anagrams():
-    pass
+def detect_anagrams(word, candidates):
+    anagram = []
+    word = [c.lower() for c in word]
+    for item in candidates:
+        symb = [c.lower() for c in item]
+        if symb != word and sorted(symb) == sorted(word):
+                anagram.append(item)
+    return anagram
