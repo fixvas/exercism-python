db2053d6f9c53bac3a964bd5680c85b2776d1725
fixvas <fixvas@gmail.com>
2017-06-24 10:24:32 +0300

    1st iteration of Difference-of-Squares

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
-def square_of_sum():
-    pass
+def square_of_sum(N):
+    return pow(sum(range(1, N + 1)), 2)
 
 
-def sum_of_squares():
-    pass
+def sum_of_squares(N):
+    return sum(map(lambda x: x**2, range(1, N + 1)))
 
 
-def difference():
-    pass
+def difference(N):
+    return abs(sum_of_squares(N) - square_of_sum(N))
