b1426cf9eb13506dd39dd82423da3b00e14e8b6a
fixvas <fixvas@gmail.com>
2017-07-01 20:40:43 +0300

    2nd iteration of Series

--- series/series.py
+++ series/series.py
@@ -2,7 +2,6 @@ def slices(digits, n):
     res = []
     if n > len(digits) or n == 0:
         raise ValueError
-    while len(digits) - n >= 0:
-        res.append([int(x) for x in digits[0:n]])
-        digits = digits[1:]
+    for i in range(len(digits) - n + 1):
+        res.append([int(char) for char in digits[i:i+n]])
     return res
