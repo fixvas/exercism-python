f6658783462695530e62d54d7b3d11b88f235bea
fixvas <fixvas@gmail.com>
2017-07-01 19:56:23 +0300

    1st iteration of Series

--- series/series.py
+++ series/series.py
@@ -1,2 +1,8 @@
-def slices():
-    pass
+def slices(digits, n):
+    res = []
+    if n > len(digits) or n == 0:
+        raise ValueError
+    while len(digits) - n >= 0:
+        res.append([int(x) for x in digits[0:n]])
+        digits = digits[1:]
+    return res
