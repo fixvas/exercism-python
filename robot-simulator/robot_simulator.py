6e2bcf46671e2f70a3c5a42a658dff492bf85c72
fixvas <fixvas@gmail.com>
2017-07-03 21:26:32 +0300

    1st iteration of Robot-Simulator

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,3 +1,38 @@
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
+
 class Robot(object):
-    def __init__(self):
-        pass
+    def __init__(self, bearing=NORTH, x=0, y=0):
+        self.bearing = bearing
+        self.x = x
+        self.y = y
+        self.coordinates = (self.x, self.y)
+
+    def turn_right(self):
+        self.bearing = (self.bearing + 1) % 4
+
+    def turn_left(self):
+        self.bearing = (self.bearing - 1) % 4
+
+    def advance(self):
+        if self.bearing == NORTH:
+            self.y += 1
+        elif self.bearing == EAST:
+            self.x += 1
+        elif self.bearing == SOUTH:
+            self.y -= 1
+        elif self.bearing == WEST:
+            self.x -= 1
+        self.coordinates = (self.x, self.y)
+
+    def simulate(self, instr):
+        for step in instr:
+            if step == 'A':
+                self.advance()
+            elif step == 'R':
+                self.turn_right()
+            elif step == 'L':
+                self.turn_left()
