fb00cb981695e71e0a52692d5617d48f92117385
fixvas <fixvas@gmail.com>
2017-07-06 14:13:27 +0300

    1st iteration of Sieve

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,13 @@
-def sieve():
-    pass
+def sieve(N):
+    res = []
+    sequence = {x: True for x in range(2, N+1)}
+    for number in sequence:
+        if sequence[number]:
+            res.append(number)
+            factor = 2
+            non_prime = number * factor
+            while non_prime <= N:
+                sequence[non_prime] = False
+                factor += 1
+                non_prime = number * factor
+    return res
