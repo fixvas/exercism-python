1f01b66338ed06d9cba9c4228f82ea94accfdb42
fixvas <fixvas@gmail.com>
2017-07-08 16:41:11 +0300

    1st iteration of Atbash-Cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,25 @@
-def encode():
-    pass
+from string import ascii_lowercase
+
+CIPHER = 'zyxwvutsrqponmlkjihgfedcba'
+TRANS_TABLE = str.maketrans(ascii_lowercase, CIPHER)
 
 
-def decode():
-    pass
+def encode(text, group=5):
+    res = []
+    line = [c for c in text.lower() if c.isalnum()]
+    cnt = 0
+    for char in line:
+        res.append(char.translate(TRANS_TABLE))
+        cnt += 1
+        if cnt % group == 0:
+            res.append(' ')
+            cnt = 0
+    return ''.join(res).rstrip()
+
+
+def decode(text):
+    res = []
+    for char in text:
+        if char.isalnum():
+            res.append(char.translate(TRANS_TABLE))
+    return ''.join(res)
