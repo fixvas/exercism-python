892bac802902578a416081ec3ca31ccc68c233a5
fixvas <fixvas@gmail.com>
2017-07-10 21:47:32 +0300

    4th iteration of Sum-of-Multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,5 @@
 def sum_of_multiples(limit, vector):
     multiples = set()
     for number in vector:
-        for m in range(0, limit, number):
-            multiples.add(m)
+        multiples.update(range(0, limit, number))
     return sum(multiples)
