5b7786642b0af44b8d2bd9b52ceef827e80342ba
fixvas <fixvas@gmail.com>
2017-07-10 16:02:48 +0300

    1st iteration of Sum-of-Multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,11 @@
-def sum_of_multiples():
-    pass
+import math
+
+
+def sum_of_multiples(limit, factor):
+    multiples = set()
+    for number in factor:
+        for multiplier in range(1, math.floor(limit / number)+1):
+            res = number * multiplier
+            if res < limit:
+                multiples.add(res)
+    return sum(multiples)
