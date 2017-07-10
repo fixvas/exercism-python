5dff763551cdc5c2d469cf552a30aee560c173f8
fixvas <fixvas@gmail.com>
2017-07-10 18:48:32 +0300

    3rd iteration of Sum-of-Multiples

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,8 +1,6 @@
 def sum_of_multiples(limit, vector):
     multiples = set()
     for number in vector:
-        m = limit / number
-        stop = int(m if m.is_integer() else m+1)
-        for factor in range(1, stop):
-            multiples.add(number * factor)
+        for m in range(0, limit, number):
+            multiples.add(m)
     return sum(multiples)
