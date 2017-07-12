f644ea8443f3db1c145f65729ee53c258290fbab
fixvas <fixvas@gmail.com>
2017-07-12 16:03:36 +0300

    1st iteration of Acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,6 @@
-def abbreviate():
-    pass
+def abbreviate(phrase):
+    phrase = phrase.replace('-', ' ')
+    acronym = ''
+    for word in phrase.split():
+        acronym += word[0].upper()
+    return acronym
